be607329bb50ec0e2ec4432666b99eab86f74a29
Andy <900808f1@gmail.com>
2025-05-09 16:41:07 +0800

    改圖標

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,8 +5,9 @@ const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          論文管理系統
+        <img src="/nycu_logo.png" alt="Logo" style={{ height: 80, marginRight: 8 }} />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold', fontSize: '1.8rem'}}>
+          CIF LAB
         </Typography>
         <Box>
           <Button
