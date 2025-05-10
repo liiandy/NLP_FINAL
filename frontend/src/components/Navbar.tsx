f335d6eeccdc85381edb4e304b43283dfa2b5ad9
Andy <900808f1@gmail.com>
2025-05-10 16:24:21 +0800

    排版

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
   const { user } = useContext(AuthContext);
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -15,40 +16,24 @@ const Navbar = () => {
           </Typography>
         </RouterLink>
         <Box>
-          <Button
-            color="inherit"
-            component={RouterLink}
-            to="/"
-          >
-            首頁
-          </Button>
-          <Button
-            color="inherit"
-            component={RouterLink}
-            to="/upload"
-          >
-            上傳論文
-          </Button>
-          <Button
-            color="inherit"
-            component={RouterLink}
-            to="/search"
-          >
-            搜索論文
-          </Button>
-          { user ? (
-            <div className="user-info" style={{ cursor: 'pointer' }}>
+          <Button color="inherit" component={RouterLink} to="/">首頁</Button>
+          <Button color="inherit" component={RouterLink} to="/upload">上傳論文</Button>
+          <Button color="inherit" component={RouterLink} to="/search">搜索論文</Button>
+          {user ? (
+            <div className="user-info" style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
               <img
                 src={user.avatar || localStorage.getItem('userAvatar') || '/default_avatar.png'}
                 alt="Avatar"
                 className="avatar"
                 style={{ width: 40, height: 40, borderRadius: '50%' }}
               />
-              <span>{user.name || user.username || localStorage.getItem('userName') || '使用者名稱'}</span>
+              <span style={{ marginLeft: 8 }}>
+                {user.name || user.username || localStorage.getItem('userName') || '使用者名稱'}
+              </span>
             </div>
           ) : (
             <Button color="inherit" component={RouterLink} to="/login">登入</Button>
-          ) }
+          )}
         </Box>
       </Toolbar>
     </AppBar>
